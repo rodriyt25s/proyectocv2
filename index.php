a77ad61d570c42673efd9b295373ff8c38ff087f
rodriyt25s <114432054+rodriyt25s@users.noreply.github.com>
2022-10-13 01:40:28 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,281 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Document</title>
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      rel="stylesheet">
+
+</head>
+<body>
+<!-------------------------------------------------------->
+
+<!-------------------------------------------------------->
+  <div class="container">
+  <nav class="navbar fixed-top navbar-expand-lg navbar-light" style="background-color: #FF3333;">
+    <div class="container-fluid">
+    <a class="navbar-brand" href="index2.html">TELMEX LA MEJOR RED</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Paquetes internet</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#estudios">Estudios</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#portafolio">Portafolio</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link " href="#intereces">servicio</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link " href="#contacto">Contacto</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<br><br><br>
+<!-------------------------------------------------------->
+<div class="col-6">
+<div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="imag/img1.jfif" class="d-block w-100" alt="">
+    </div>
+    <div class="carousel-item">
+      <img src="imag/img4.jfif" class="d-block w-100" alt="">
+    </div>|
+    <div class="carousel-item">
+      <img src="imag/img6.jfif" class="d-block w-100" alt="">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div></div>
+<br>
+
+<!-------------------------------------------------------->
+<div id="nosotros" class="row">
+	<div class="col-12" align="center" style="background-color: darkred;">
+	<br>
+	<h1><i>TELMEX LA MEJOR RED</i></h1>
+	<h2>se parte del equipo telmex UwU</h2>
+	<br>
+	</div>
+</div>
+<br>
+<!-------------------------------------------------------->
+
+<div class="row">
+	<div class="col-6">
+		<div class="container">
+			<div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="imag/img4.jfif" class="d-block w-100" alt="">
+    </div>
+    <div class="carousel-item">
+      <img src="imag/img6.jfif" class="d-block w-100" alt="">
+    </div>
+    <div class="carousel-item">
+      <img src="imag/img5.JFIF" class="d-block w-100" alt="">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+		</div>
+	</div>
+
+	<div class="col-6">
+		<div class="accordion" id="accordionExample">
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingOne">
+      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+        Mision
+      </button>
+    </h2>
+    <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        <strong>Nuestra Mision </strong> La misión de una empresa se define como la razón principal por la cual esta existe, es decir, cuál es su propósito u objetivo y cuál es su función dentro de la sociedad <code>.accordion-body</code>, though the transition does limit overflow.
+      </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingTwo">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+        Vision
+      </button>
+    </h2>
+    <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        <strong>Nuestra Vision </strong> La Visión es la capacidad de ver más allá, en tiempo y espacio, y por encima de los demás, significa visualizar, ver con los ojos de la imaginación, en términos del resultado final que se pretende alcanzar.
+      </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingThree">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+        Valores
+      </button>
+    </h2>
+    <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        <strong>Nuestros valores</strong> 
+        <ol>
+        	<li>Responsabilidad</li>
+        	<li>Honestidad</li>
+          <li>Confianza</li>
+          <li>Respeto</li>
+        </ol>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+</div>
+<!-------------------------------------------------------->
+<div id="servicios" class="row">
+	<div class="col-12" align="center">
+	<br>
+	<h1><i>Servicios</i></h1>
+	<br>
+	</div>
+</div>
+<!-------------------------------------------------------->
+<div class="row">
+	<div class="col-4">
+   <div class="card" style="width: 18rem;">
+    <img src="imag/img3.jfif" class="card-img-top" alt="Instalaciones deportivas">
+    <div class="card-body">
+      <h5 class="card-title">paquete chico</h5>
+      <p class="card-text">contamos con un paquete pequeño pero con la posibilidad de navegar con aplicaciones de bajo consumo para mayor rendimiento</p>
+    </div>
+  </div> 
+</div>
+
+	<div class="col-4">
+    <div class="card" style="width: 18rem;">
+    <img src="imag/img8.jfif" class="card-img-top" alt="Instalaciones deportivas">
+    <div class="card-body">
+      <h5 class="card-title">paquetes medios</h5>
+      <p class="card-text">Contamos con unos paquetes con mejor calidad para lo que sea,con este paquete se podra navegar y podras tener una buena velocidad para yotube,musica,peliculas etc.</p>
+    </div>
+  </div> 
+  </div>
+	<div class="col-4">
+   <div class="card" style="width: 18rem;">
+    <img src="imag/img2.jfif" class="card-img-top" alt="Instalaciones deportivas">
+    <div class="card-body">
+      <h5 class="card-title">paquete alto</h5>
+      <p class="card-text">Contamos con el mejor paquete para ti con el cual te ofrecemos la mejor velocidad al momento de tener una conexion y con ella podras jugar sin preocuparte de tener retardos "lag"y ser el mejor.</p>
+    </div>
+  </div>  
+  </div>
+
+</div>
+<br><br>
+<div class="row">
+  <div class="col-4">
+   <div class="card" style="width: 18rem;">
+    <img src="imag/img6.jpeg" class="card-img-top" alt="Instalaciones deportivas">
+    <div class="card-body">
+      <h5 class="card-title">Instalaciones deportivas</h5>
+      <p class="card-text">Contamos con unas amplias instalaciones deportivas cómo son futbol, basquetbol, voliebol, gymnasio al area libre para su preferencia .</p>
+    </div>
+  </div> 
+</div>
+
+  <div class="col-4">
+    <div class="card" style="width: 18rem;">
+    <img src="imag/img6.jpeg" class="card-img-top" alt="Instalaciones deportivas">
+    <div class="card-body">
+      <h5 class="card-title">Instalaciones deportivas</h5>
+      <p class="card-text">Contamos con unas amplias instalaciones deportivas cómo son futbol, basquetbol, voliebol, gymnasio al area libre para su preferencia .</p>
+    </div>
+  </div> 
+  </div>
+  <div class="col-4">
+   <div class="card" style="width: 18rem;">
+    <img src="imag/img6.jpeg" class="card-img-top" alt="Instalaciones deportivas">
+    <div class="card-body">
+      <h5 class="card-title">Instalaciones deportivas</h5>
+      <p class="card-text">Contamos con unas amplias instalaciones deportivas cómo son futbol, basquetbol, voliebol, gymnasio al area libre para su preferencia .</p>
+    </div>
+  </div>  
+  </div>
+</div>
+<!-------------------------------------------------------->
+<div id="contacto" class="container">
+  <div class="row">
+    <div class="col-12 text-center">
+      <h1 class="display-1">Contacto</h1>
+      <hr>
+      <br><br><br>
+      <span class="material-icons-outlined">call:</span>227-227-6789<br>
+      <span class="material-icons-outlined">place:</span>Santa Ana Xalmimilulco<br>
+      <span class="material-icons-outlined">email:</span>3520110817@uth.edu.mx</span><br>
+      <span class="material-icons-outlined">access_alarm</span><br>
+      <br>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d941.9209574205305!2d-98.39114663196172!3d19.20900698640802!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85cfd1f35ec2d3fb%3A0xee133cee4b1c87ef!2sUniversidad%20Tecnol%C3%B3gica%20de%20Huejotzingo!5e0!3m2!1ses!2smx!4v1656092569986!5m2!1ses!2smx" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
+  </div>
+</div>
+
+<!-------------------------------------------------------->
+
+<div class="card mb-3" style="max-width: 540px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="..." class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+         <div class="carousel-item">
+      <img src="img1/img.JPG" class="d-block w-100" alt="">
+    </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+</div>
+<script src="js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
---
+++ index2.php
@@ -0,0 +1,248 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>CV Personal</title>
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      rel="stylesheet">
+
+</head>
+<body>
+	<!-------------------------------------------------------->
+	<div class="container">
+	<nav class="navbar fixed-top navbar-expand-lg navbar-light" style="background-color: #FF3333;">
+  	<div class="container-fluid">
+    <a class="navbar-brand" href="index2.html">el gera vale verga</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Paquetes internet</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#estudios">Estudios</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#portafolio">Portafolio</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link " href="#intereces">Intereses</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link " href="#contacto">Contacto</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<br><br><br>
+<!-------------------------------------------------------->
+<div class="row">
+	<div class="col-6" align="center" style="background-color: #33FFEC ;"> 
+<h5>los mejores paquetes a los mejores precios de el pais con la mejor calidad</h5>
+<figure class="figure">
+  <img src="imag/foto1.jpg" class="figure-img img-fluid rounded" alt="" width="50%"></div>
+  <div class="col-6" style="background-color: #4633FF;">
+  		<br><br><br>
+  		<br><br>
+  <h4 align="center">con la mejor red tendras los mejores servicios y paquetes para navegar con la mejor red con la cual podras jugar con la mejor velocidad.</h4>
+</figure>
+</div>
+</div>
+<br>
+<!-------------------------------------------------------->
+		<div class="row">
+  <div class="col-6">
+    <div class="container">
+      <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="imag/img4.jfif" class="d-block w-100" alt="">
+    </div>
+    <div class="carousel-item">
+      <img src="imag/img1.png" class="d-block w-100" alt="">
+    </div>
+    <div class="carousel-item">
+      <img src="imag/img6.jfif" class="d-block w-100" alt="">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+    </div>
+  </div>
+  <!------------------------------------------------>
+
+  <div class="col-6">
+    <div class="accordion" id="accordionExample">
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="heading1">
+      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse1" aria-expanded="true" aria-controls="collapse1">
+        Secundaria
+      </button>
+    </h2>
+    <div id="collapse1" class="accordion-collapse collapse show" aria-labelledby="heading1" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        <strong></strong> La misión de una empresa se define como la razón principal por la cual esta existe, es decir, cuál es su propósito u objetivo y cuál es su función dentro de la sociedad <code>
+      </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="heading2">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse2" aria-expanded="false" aria-controls="collapse2">Nombre de la Institución educativa</button>
+    </h2>
+    <div id="collapse3" class="accordion-collapse collapse" aria-labelledby="heading3" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        <strong> </strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+      </div>
+    </div>
+  </div>
+ <div class="accordion-item">
+    <h2 class="accordion-header" id="heading4">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse4" aria-expanded="false" aria-controls="collapse4">Bachillerato</button>
+    </h2>
+    <div id="collapse4" class="accordion-collapse collapse" aria-labelledby="heading4" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        <strong> </strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+      </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="heading5">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse5" aria-expanded="false" aria-controls="collapse5">TSU</button>
+    </h2>
+    <div id="collapse5" class="accordion-collapse collapse" aria-labelledby="heading5" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        <strong></strong> Planear y administrar el desarrollo de Sistemas de Información. Proponer soluciones informáticas y metodologías adecuadas a la organización. Promover el uso adecuado de las tecnologías de información nuevas y vigentes, dentro de su comunidad o grupos de trabajo donde se encuentre colaborando o asesorando.
+
+      </div>
+    </div>
+  </div>
+
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="heading5">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse5" aria-expanded="false" aria-controls="collapse5">Cursos</button>
+    </h2>
+    <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        <strong> </strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+      </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingTwo">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">Conocimientos y Habilidades</button>
+    </h2>
+    <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        <strong></strong> para ello debemos de tener habilidades con respecto al campo que nos vamos a especializar en este caso podria ser "lenjuajes de programacion"
+      </div>
+    </div>
+  </div>
+      </div>
+    </div>
+  </div>
+</div>
+<br><br><br><br>
+<!--------------------------------------------------------->
+<div id="portafolio" class="container">
+  <div class="row">
+    <div class="col-12 text-center">
+      <h1 class="display-1">Portafolio</h1>
+      <hr>
+<div class="card mb-3" style="max-width: 540px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="..." class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+</div>
+</div>
+<!---------------------------------------->
+
+
+<div id="intereces" class="row">
+  <div class="col-12" align="center">
+  <br>
+  <h1 class="display-1">Intereses</h1>
+      <hr>
+  <br>
+  </div>
+</div>
+<div class="row">
+  <div class="col-4" align="center">
+   <div class="card" style="width: 18rem;">
+    <img src="imag/img2.jfif" class="card-img-top" alt="Instalaciones deportivas">
+    <div class="card-body">
+      <h5 class="card-title">Lectura</h5>
+      <p class="card-text">Contamos con unas amplias instalaciones deportivas cómo son futbol, basquetbol, voliebol, gymnasio al area libre para su preferencia .</p>
+    </div>
+  </div> 
+</div>
+
+  <div class="col-4" align="center">
+    <div class="card" style="width: 18rem" align="center";>
+    <img src="imag/img1.jfif" class="card-img-top" alt="Instalaciones deportivas">
+    <div class="card-body">
+      <h5 class="card-title">Deportes</h5>
+      <p class="card-text">Contamos con unas amplias instalaciones deportivas cómo son futbol, basquetbol, voliebol, gymnasio al area libre para su preferencia .</p>
+    </div>
+  </div> 
+  </div>
+  <div class="col-4" align="center">
+   <div class="card" style="width: 18rem;">
+    <img src="imag/img3.jfif" class="card-img-top" alt="Instalaciones deportivas">
+    <div class="card-body">
+      <h5 class="card-title">Instalaciones deportivas</h5>
+      <p class="card-text">Contamos con unas amplias instalaciones deportivas cómo son futbol, basquetbol, voliebol, gymnasio al area libre para su preferencia .</p>
+    </div>
+  </div>  
+  </div>
+</div>
+
+<br><br><br><br>
+<!----------------------------------------------->
+<div id="contacto" class="container">
+  <div class="row">
+    <div class="col-12 text-center">
+      <h1 class="display-1">Contacto</h1>
+      <hr>
+      <br><br><br>
+      <span class="material-icons-outlined">call:</span>222-222-3746<br>
+      <span class="material-icons-outlined">place:</span>Santa Ana Xalmimilulco<br>
+      <span class="material-icons-outlined">email</span></span><br>
+      <span class="material-icons-outlined">access_alarm</span><br>
+      <br>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d941.9209574205305!2d-98.39114663196172!3d19.20900698640802!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85cfd1f35ec2d3fb%3A0xee133cee4b1c87ef!2sUniversidad%20Tecnol%C3%B3gica%20de%20Huejotzingo!5e0!3m2!1ses!2smx!4v1656092569986!5m2!1ses!2smx" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
+  </div>
+</div>
+
+
+<script src="js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
